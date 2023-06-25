2dcab648bb4718d820b59f843179a6c8a750a1ec
Steve Long <stevelong@prpl.rs>
2023-06-24 22:41:35 -0500

    update file path

--- src/hooks/useMarkdownContent.tsx
+++ src/hooks/useMarkdownContent.tsx
@@ -5,7 +5,7 @@ export const useMarkdownContent = () => {
     const [content, setContent] = React.useState('');
 
     const getFileData = async () => {
-        await fetch('/markdown/file1.md').then((response) => response.body.getReader().read()).then(c => {
+        await fetch('./markdown/file1.md').then((response) => response.body.getReader().read()).then(c => {
             const mdStr = new TextDecoder().decode(c.value);
             setContent(mdStr);
         });
