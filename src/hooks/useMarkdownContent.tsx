2f4702778a342b0e910576d6338dfc8e8c510633
Steve Long <stevelong@prpl.rs>
2023-06-25 16:16:33 -0500

    use api to fetch content

--- src/hooks/useMarkdownContent.tsx
+++ src/hooks/useMarkdownContent.tsx
@@ -4,18 +4,65 @@ export const useMarkdownContent = (): [string, React.Dispatch<React.SetStateActi
 
     const [content, setContent] = React.useState('');
 
-    const getFileData = async () => {
-        await fetch('./markdown/file1.md').then((response) => response.body.getReader().read()).then(c => {
-            const mdStr = new TextDecoder().decode(c.value);
-            setContent(mdStr);
+    const FetchSingleDocContent = async () => {
+        
+        const apiUrl = '//localhost:3000/documents/6498a352835cd46dc03dbf76';
+
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data?.content) {
+                setContent(data.content);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
     }
 
     React.useEffect(() => {
         if (!content) {
-            getFileData();
+            FetchSingleDocContent();
         }
     }, []);
 
     return [content, setContent];
 }
+
+
+        // // Data to be sent
+        // const data = {
+        //     "_id": "6498a352835cd46dc03dbf76",
+        //     "__v": 1,
+        //     "content": "# h1 Heading 8-)",
+        //     "title": "example-1"
+        // };
+        
+        // // Fetch options
+        // const options = {
+        //   method: 'PUT',
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify(data)
+        // };
+        
+        // // Send the PUT request
+        // fetch(apiUrl, options)
+        //   .then(response => {
+        //     if (response.ok) {
+        //       // Request was successful
+        //       console.log('Data successfully written via PUT.');
+        //     } else {
+        //       // Request was not successful
+        //       console.error('Error writing data via PUT:', response.status, response.statusText);
+        //     }
+        //   })
+        //   .catch(error => {
+        //     console.error('Error writing data via PUT:', error);
+        //   });
