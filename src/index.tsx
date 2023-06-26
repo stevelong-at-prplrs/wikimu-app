537b8d6fbb85848f7697a892e59cea0c4c660175
Steve Long <stevelong@prpl.rs>
2023-06-26 08:38:55 -0500

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "./app";
 
-const docContext: 'viewing' | 'editing' = 'editing';
+const params = new URLSearchParams(window.location.search);
+const docContext: 'viewing' | 'editing' = params.has("editing") ? 'editing' : 'viewing'; // make this into a tab selector?
 const root = createRoot(document.getElementById("app"));
 root.render(<App docContext={docContext}/>);
+
+// need a folder view of files
