8828673a2bd332aa15a636a4ad07d1d01c45a3af
Steve Long <stevelong@prpl.rs>
2023-06-29 01:19:33 -0500

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppRoot } from "./app-root";
 import { ViewDocument } from "./components/ViewDocument";
 
-// const id = '6498a352835cd46dc03dbf76';
-
-// const params = new URLSearchParams(window.location.search);
-// const docContext: 'viewing' | 'editing' = params.has("editing") ? 'editing' : 'viewing'; // make this into a tab selector?
-// const [docContext, setDocContext] = React.useState('viewing');
-
-// const docContext: 'viewing' | 'editing' = 'viewing';
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -25,5 +17,3 @@ const router = createBrowserRouter([
 
 const root = createRoot(document.getElementById("app"));
 root.render(<RouterProvider router={router} />);
-
-// need a folder view of files
