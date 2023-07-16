e8ab41718d6a1b999f5f2a164859b5294a70db26
stevelong-at-prplrs <76882223+stevelong-at-prplrs@users.noreply.github.com>
2023-07-16 08:50:48 -0500

    add SCSS and eslint (#3)
    * context menu
    
    * getting SCSSy
    
    * add eslint
    
    * add eslint
    
    ---------
    
    Co-authored-by: Steve Long <stevelong@prpl.rs>

--- src/app-root.tsx
+++ src/app-root.tsx
@@ -2,9 +2,14 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { FetchDocuments, createDoc, deleteDoc, duplicateDoc } from "./utils/api";
 import { downloadButton } from "./utils/download";
+// import ContextMenu from "./components/ContextMenu";
+import "./scss/style.scss";
+
 export const AppRoot = () => {
 
     const [docs, setDocs] = React.useState([]);
+    const [showMenu, setShowMenu] = React.useState(false);
+    const [menuPosition, setMenuPosition] = React.useState({ x: 0, y: 0 });
 
     React.useEffect(() => {
         if (docs.length === 0) {
@@ -12,11 +17,17 @@ export const AppRoot = () => {
         }
     }, []);
 
+    const handleContextMenu = (e) => {
+        e.preventDefault();
+        setShowMenu(true);
+        setMenuPosition({ x: e.clientX, y: e.clientY });
+      };
+
     return <>
     {docs.length > 0 ?
         docs.map((doc, index) => 
-            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", overflow: "hidden", flexWrap: "nowrap" }} key={index}>
-                <span style={{ textWrap: "nowrap", overflow: "hidden", "text-overflow": "ellipsis" } /* the type system is not adequate currently but this error will disapear when this inline style is moved to stylesheet */}>
+            <div onContextMenu={handleContextMenu} style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", overflow: "hidden", flexWrap: "nowrap" }} key={index}>
+                <span style={{ flexWrap: "nowrap", overflow: "hidden", textOverflow: "ellipsis" } /* the type system is not adequate currently but this error will disapear when this inline style is moved to stylesheet */}>
                     <Link key={index} to={"/" + doc._id}>{doc.title || "untitled"}</Link>
                     <span> (v{doc.__v}) {doc.content ? doc.content.length > 25 ? doc.content.substring(0, 24) + "..." : doc.content : ""}</span>
                 </span>
@@ -29,7 +40,25 @@ export const AppRoot = () => {
                 </div>
             </div>)
         : <>No docs found</>}
+        {/* {showMenu && <ContextMenu top={menuPosition.y} left={menuPosition.x} />} */}
         <br />
         <button onClick={() => createDoc()}>Add new doc</button>
         </>;
 }
+
+// const ContextMenu = ({ top, left }) =>           <div
+// style={{
+//   position: 'absolute',
+//   top,
+//   left,
+//   backgroundColor: 'white',
+//   boxShadow: '0 2px 5px rgba(0, 0, 0, 0.15)',
+//   padding: '5px',
+// }}
+// >
+// {/* Context menu content */}
+// <div>move</div>
+// <div>delete</div>
+// <div>duplicate</div>
+// <div>download</div>
+// </div>
